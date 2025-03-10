7d2cdc1e6676aa0d2e803a0d05bec585685890eb
keemsebeen <sebin0580@gmail.com>
2025-03-11 00:47:09 +0900

    refactor: Layout 컴포넌트 children 제거

--- src/main.js
+++ src/main.js
@@ -9,10 +9,16 @@ import { LunchList, LunchItem, LunchForm } from "./components/feature/index.js";
 
 addEventListener("load", () => {
   const layout = new Layout();
-  const header = new Header();
-
-  const lunchList = new LunchList();
   const bottomSheet = new BottomSheet();
+
+  const header = layout.addChild(Header);
+  const lunchList = layout.addChild(LunchList);
+
+  header.setProps({
+    title: "점심 뭐먹지",
+    iconName: "add-button",
+    onIconClick: () => bottomSheet.open(),
+  });
 
   bottomSheet.setProps({
     onAdd: (data) => {
@@ -22,18 +28,7 @@ addEventListener("load", () => {
     },
   });
 
-  const lunchForm = new LunchForm();
   bottomSheet.addChild(LunchForm);
-
-  header.setProps({
-    title: "점심 뭐먹지",
-    iconName: "add-button",
-    onIconClick: () => bottomSheet.open(),
-  });
-
-  layout.setProps({
-    children: [header, lunchList],
-  });
 
   layout.render();
 });
